ac44fc6566d8a9444d6d22d9d587dfd14824c2c7
Max Metral <max@pyralis.com>
2017-01-22 16:46:13 -0500

    Update proxy

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,43 @@
+import os from 'os';
+import url from 'url';
 import http from 'http';
 import https from 'https';
 
-function monkeyPatch(module, functionName, newFunction) {
-  module[functionName] = newFunction.bind(undefined, module[functionName]);
-}
+const originalRequest = http.request;
+const originalHttps = https.request;
 
 export default class Proxy {
   constructor(context, config) {
-
+    this.name = context.service ? context.service.name : os.hostname();
   }
 
   start(context) {
-    monkeyPatch(http, 'request', function (originalRequest, options, callback) {
-      console.error(options);
-      return originalRequest(options, callback);
-    });
+    http.request = (options, callback) => {
+      this.rewire(options, 'http', 80);
+      return originalRequest.call(http, options, callback);
+    };
+    https.request = (options, callback) => {
+      if (this.rewire(options, 'https', 443)) {
+        return originalRequest.call(http, options, callback);
+      }
+      return originalHttps.call(https, callback);
+    };
   }
 
   stop(context) {
 
+  }
+
+  rewire(options, protocol, defPort) {
+    if (options.host.match(/[^0-9.]/)) {
+      options.headers = options.headers || {};
+      options.headers.host = `${protocol}.${options.host}.${options.port || defPort}`;
+      options.headers.source = this.name;
+      options.host = 'container-proxy';
+      options.port = 9990;
+      options.protocol = 'http:';
+      return true;
+    }
+    return false;
   }
 }
